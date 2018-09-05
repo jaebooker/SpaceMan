0c306cdf7aa3bb749733ea0cff92d65321fd4960
jaebooker <jaesonbooker@gmail.com>
2018-09-05 14:26:41 -0700

    trimmed it down

--- spaceman.py
+++ spaceman.py
@@ -2,7 +2,7 @@ word = ["S","P","A","C","E","M","A","N"]
 word_reveal = ["","","","","","","",""]
 spaceman = 0
 count = 0
-while (count < 8) and (spaceman < 7):
+while (count < len(word_reveal)) and (spaceman < 7):
     word1 = raw_input("Enter a letter: ").upper()
     counter = 0
     for i in range(0, len(word)):
@@ -13,7 +13,4 @@ while (count < 8) and (spaceman < 7):
             counter += 1
     if counter == len(word)-1:
         spaceman += 1
-        print(spaceman)
     print(word_reveal)
-    print(count)
-    print(spaceman)
